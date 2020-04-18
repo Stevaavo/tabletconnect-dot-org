c838751194362e6ec8f7c43903391465f9b86959
Stevaavo <34781969+Stevaavo@users.noreply.github.com>
2020-04-18 19:10:35 -0400

    Added suggestion to leave plugged in when sending/giving the tablet.  Also more specific suggestion on disinfection.

--- components/ShipInstructions.js
+++ components/ShipInstructions.js
@@ -63,14 +63,21 @@ export default class Home extends React.Component {
                     </p>
 
                     <p>
-                        If you are dropping off the tablet at your relative/friend's home, or at a hospital/rehab front desk, you might consider leaving the tablet on when you do so.  
-                        If it's already on when it reaches your relative/friend, then all they need to do is plug it in. 
+                        If you are dropping off the tablet at your relative/friend's home, or at a hospital/rehab front desk, you might consider leaving the tablet on and plugged into its charger when you do so.  
+                        Then all they need to do is plug the charger into an electrical outlet. 
                         Tablets have larger batteries than phones, and can usually stay on for several days between charges, if they are not in active use. 
                     </p>
 
                     <p>
-                        I would also suggest <strong className="highlight">disinfecting</strong> the tablet before you ship it, since coronavirus can survive for a long time on glass and plastic surfaces.  You can
-                        find a guide to disinfecting tablets and smartphones <TrackedLink href="https://thewirecutter.com/blog/disinfect-your-phone-coronavirus/">here</TrackedLink>.
+                        If you are shipping the tablet, you may want to leave the tablet plugged into its charger when you package it. 
+                        Just make sure to box it up in a way that no pressure will be placed on the charging connector when the package is kicked around in transit. 
+                        Here is a <TrackedLink href="/static/packaging.png">photo of an example packaging arrangement</TrackedLink>.
+                    </p>
+
+                    <p>
+                        Either way, <strong className="highlight">make sure to disinfect</strong> the tablet before you ship it, since coronavirus can survive for a long time on glass and plastic surfaces.  You can
+                        find a guide to disinfecting tablets and smartphones <TrackedLink href="https://thewirecutter.com/blog/disinfect-your-phone-coronavirus/">here</TrackedLink>. 
+                        In my case, I wet a paper towel with Lysol, and then wiped down the tablet, the charger, and the inside of the packaging that I used to ship it all.
                     </p>
 
                 </div>
